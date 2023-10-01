35c25c803382760288a2b8599c7ec9541492a95b
Frederic MUHLACH <frederic.muhlach@gmail.com>
2023-10-01 18:31:01 +0200

    Migration vers une base de données POSTGRESQL

--- src/Mercadona/settings.py
+++ src/Mercadona/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Mercadona.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mercadonadb',
+        'USER': 'mercadona_user',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
