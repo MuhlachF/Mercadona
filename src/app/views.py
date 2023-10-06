81837ba915443399854f68ccccbd177b1979e891
Frederic MUHLACH <frederic.muhlach@gmail.com>
2023-10-06 11:52:44 +0200

    Développement de l'interface FrontEnd

--- src/app/views.py
+++ src/app/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 from django.views.generic import ListView
 
-from app.models import Article
+from app.models import Article, Category
 
 
 class AppHome(ListView):
-    paginate_by = 3  # Affiche 10 articles par page
+    paginate_by = 10  # Affiche 10 articles par page
+
     model = Article
     context_object_name = "Articles"
+
+    # Surcharge de la méthode pour y inclure les catégories
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Ajouter les catégories au contexte
+        context['Categories'] = Category.objects.all()
+
+        return context
