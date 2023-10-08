2504bc5180a2eb4b88efe62258bb3213250054fc
Frederic MUHLACH <frederic.muhlach@gmail.com>
2023-10-08 21:30:22 +0200

    Finalisation du Frontend - pagination OK

--- src/app/static/script/load_article.js
+++ src/app/static/script/load_article.js
@@ -1,3 +1,6 @@
+let currentPage = 1; // Page courante
+let totalPages = 1; // Nombre total de pages, initialisé à 1
+
 // Fonction pour tronquer une chaîne de caractères à un certain nombre de mots 
 function truncateWords(str, no_words) {
     let mots = str.split(" ");
@@ -15,13 +18,16 @@ function truncateWords(str, no_words) {
 }
 
 // Fonction pour charger des articles depuis l'API
-function loadArticles(category) {
+function loadArticles(category, page = 1) {
     // Faire une requête fetch pour obtenir des articles depuis l'API
-    fetch(`/api2/get_articles/?category=${category}`)
+    fetch(`/api2/get_articles/?category=${category}&page=${page}`)
         .then(response => response.json())
         .then(data => {
             const articleTableBody = document.getElementById('articleTable');
             articleTableBody.innerHTML = ''; // Effacer le contenu existant du tbody
+
+            totalPages = Math.ceil(data.count / data.resultatPerPage); // Calcule le nombre total de pages
+            currentPage = page; // Met à jour la page courante
 
             // Parcourir chaque article et créer une ligne dans le tableau
             console.log(data)
@@ -57,11 +63,42 @@ function loadArticles(category) {
                     `;
                 articleTableBody.appendChild(row);
             });
+            // Mettre à jour l'état des boutons de pagination
+            const prevPageButton = document.getElementById('prevPage');
+            const nextPageButton = document.getElementById('nextPage');
 
-            ;
+            if (currentPage <= 1) {
+                prevPageButton.setAttribute("disabled", "disabled");
+                prevPageButton.classList.add("disabled");
+            } else {
+                prevPageButton.removeAttribute("disabled");
+                prevPageButton.classList.remove("disabled");
+            }
+
+            if (currentPage >= totalPages) {
+                nextPageButton.setAttribute("disabled", "disabled");
+                nextPageButton.classList.add("disabled");
+            } else {
+                nextPageButton.removeAttribute("disabled");
+                nextPageButton.classList.remove("disabled");
+            }
+
         });
 
 }
+
+function prevPage() {
+    if (currentPage > 1) {
+        loadArticles('', currentPage - 1);
+    }
+}
+
+function nextPage() {
+    if (currentPage < totalPages) {
+        loadArticles('', currentPage + 1);
+    }
+}
+
 
 // Code qui s'exécute lorsque le DOM est complètement chargé
 document.addEventListener('DOMContentLoaded', () => {
